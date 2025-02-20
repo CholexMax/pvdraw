b9c35679a76381372e2260f8374cd7da7c74f919
cholexMax <tobexnewlife@gmail.com>
2025-02-20 20:17:44 +0530

    canvas component create

---
+++ pvdraw/src/components/Canvas.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Canvas() {
+  return (
+    <canvas width="1200" height="700" style={{zIndex:1,backgroundColor:"red",cursor:"crosshair"}}></canvas>
+  )
+}

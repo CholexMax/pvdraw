7f5d2c19ab14f7b1c0a0ce035d95501e651071c9
cholexMax <tobexnewlife@gmail.com>
2025-02-20 20:20:41 +0530

    custome attribues and new props add

--- pvdraw/src/components/Tool.jsx
+++ pvdraw/src/components/Tool.jsx
@@ -1,8 +1,8 @@
 function ToolIcon(props)
 {
     return (
-        <div className="flex items-center justify-center cursor-pointer p-2 hover:bg-teal-100 hover:border-blue-200 hover:border-[1px]">
-            <img src={props.src} alt={props.description} width="15px" />
+        <div className={` ${props.isActive?"bg-blue-800":"bg-gray-200"} flex items-center justify-center cursor-pointer p-2 hover:bg-teal-100 hover:border-blue-200 hover:border-[1px]`} onClick={props.detect}>
+            <img src={props.src} alt={props.description} width="15px" id={props.id} data-isactive={props.isActive}/>
         </div>
     )
 }

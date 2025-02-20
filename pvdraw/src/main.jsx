7bf848a9a94e22c48c510761095d79a28139cdc6
cholexMax <tobexnewlife@gmail.com>
2025-02-20 17:14:15 +0530

    rendring simple app ui

--- pvdraw/src/main.jsx
+++ pvdraw/src/main.jsx
@@ -2,9 +2,19 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { RouterProvider,Route,Router,createBrowserRouter } from 'react-router-dom'
 
+
+const route=createBrowserRouter(
+  [
+    {
+      path:"/",
+      element:<App/>
+    }
+  ]
+)
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={route}></RouterProvider>
   </StrictMode>,
 )

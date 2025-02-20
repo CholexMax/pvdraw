234461aee4d678534149cb638dc8c3713846d411
cholexMax <tobexnewlife@gmail.com>
2025-02-20 20:19:09 +0530

    wrapped things with provider

--- pvdraw/src/App.jsx
+++ pvdraw/src/App.jsx
@@ -1,19 +1,35 @@
+import Canvas from "./components/Canvas"
 import DrawingStyle from "./components/DrawingStyle"
 import ToolContainer from "./components/ToolContainer"
+import { DrawingDataProvider } from "./contexts/Drawing"
+import { StyleProvider } from "./contexts/Style"
+import { toolProvider } from "./contexts/Tool"
+
 
 function App() {
-
+  
   return (
-    <>
-    <ToolContainer/>
-    <div className="w-[200px] h-72  overflow-y-scroll" style={
-      {
-        scrollbarWidth:"thin"
-      }
-    }>
-    <DrawingStyle/>
+    <toolProvider>
+    <div className="relative">
+    <StyleProvider>
+  <DrawingDataProvider>
+  <Canvas />
+   </DrawingDataProvider>
+   <ToolContainer />
+    <div
+      className="w-[200px] h-72 overflow-y-scroll"
+      style={{
+        scrollbarWidth: "thin",
+        zIndex: 2,
+        backgroundColor: "#f8f8f8", // Optional for visibility
+        padding: "10px",
+      }}
+    >
+      <DrawingStyle />
     </div>
-    </>
+    </StyleProvider>
+  </div>
+  </toolProvider>
   )
 }
 

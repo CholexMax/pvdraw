dd4436e47cb6460cb113d1dc88877750d6faf7b6
cholexMax <tobexnewlife@gmail.com>
2025-02-21 12:53:15 +0530

    canvas demo add

--- pvdraw/src/components/Canvas.jsx
+++ pvdraw/src/components/Canvas.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import { useRef } from 'react'
+import { useContext } from 'react'
+import { toolSeclection } from '../contexts/Tool'
 
 export default function Canvas() {
+  const {tool}=useContext(toolSeclection)
+  const canvasRef=useRef(null)
+  const draw=() => {
+   if(tool[0].selection==="Circle")
+    {
+      let ctx=canvasRef.current.getContext("2d")
+      ctx.fillStyle = 'blue'; // Set fill color
+      ctx.fillRect(50, 25, 200, 100); // Draw the rectangle
+      console.log("called ");
+      
+    }
+  }
   return (
-    <canvas width="1200" height="700" style={{zIndex:1,backgroundColor:"red",cursor:"crosshair"}}></canvas>
+    <canvas width="1200" ref={canvasRef} height="700" onClick={draw} style={{zIndex:1,backgroundColor:"red",cursor:"crosshair"} }></canvas>
   )
 }
